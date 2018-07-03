428d9c573934e172616d66ad5970b72551aef985
Travis McCormick <tmccorm.dev@gmail.com>
2018-07-03 09:09:47 -0600

    Added rounded cells

---
+++ Filo-FinalUdacityProject/View/CoinCellRounded.swift
@@ -0,0 +1,18 @@
+//
+//  CoinCellRounded.swift
+//  Filo-FinalUdacityProject
+//
+//  Created by Travis McCormick on 6/29/18.
+//  Copyright © 2018 Travis McCormick. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class cellRounded: UIView {
+    override var bounds: CGRect {
+        didSet {
+          //  setUpShadow()
+        }
+    }
+}
